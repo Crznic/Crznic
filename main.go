068a3509a2dcb1584e732355ce74cfc8f552a500
Chris Vantine <justchris.vantine@gmail.com>
2018-04-18 23:22:13 -0400

    Functionized syn packets

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 // get the local ip and port based on our destination ip
-func localIPPort(dstip net.IP) (net.IP, int) {
+func localIPPort(dstip net.IP) (net.IP, layers.TCPPort) {
 	serverAddr, err := net.ResolveUDPAddr("udp", dstip.String()+":12345")
 	if err != nil {
 		log.Fatal(err)
@@ -21,58 +21,36 @@ func localIPPort(dstip net.IP) (net.IP, int) {
 	// based on our destination ip what source ip we should use.
 	if con, err := net.DialUDP("udp", nil, serverAddr); err == nil {
 		if udpaddr, ok := con.LocalAddr().(*net.UDPAddr); ok {
-			return udpaddr.IP, udpaddr.Port
+			tcpPort := layers.TCPPort(udpaddr.Port)
+			return udpaddr.IP, tcpPort
 		}
 	}
 	log.Fatal("could not get local ip: " + err.Error())
 	return nil, -1
 }
 
-func main() {
-	if len(os.Args) != 3 {
-		log.Printf("Usage: %s <host/ip> <port>\n", os.Args[0])
-		os.Exit(-1)
-	}
-	log.Println("starting")
-
-	dstaddrs, err := net.LookupIP(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// parse the destination host and port from the command line os.Args
-	dstip := dstaddrs[0].To4()
-	var dstport layers.TCPPort
-	if d, err := strconv.ParseUint(os.Args[2], 10, 16); err != nil {
-		log.Fatal(err)
-	} else {
-		dstport = layers.TCPPort(d)
-	}
-
-	srcip, sport := localIPPort(dstip)
-	srcport := layers.TCPPort(sport)
+// builds and sends a tcp packet
+func sendSyn(dstip net.IP, dstport layers.TCPPort, seq uint32) (ip *layers.IPv4, tcp *layers.TCP) {
+	srcip, srcport := localIPPort(dstip)
 	log.Printf("using srcip: %v", srcip.String())
 
 	// Our IP header... not used, but necessary for TCP checksumming.
-	ip := &layers.IPv4{
+	ip = &layers.IPv4{
 		SrcIP:    srcip,
 		DstIP:    dstip,
 		Protocol: layers.IPProtocolTCP,
 	}
+
 	// Our TCP header
-	tcp := &layers.TCP{
+	tcp = &layers.TCP{
 		SrcPort: srcport,
 		DstPort: dstport,
-		Seq:     1105024978,
+		Seq:     seq,
 		SYN:     true,
 		Window:  14600,
 	}
 	tcp.SetNetworkLayerForChecksum(ip)
 
-	// Serialize.  Note:  we only serialize the TCP layer, because the
-	// socket we get with net.ListenPacket wraps our data in IPv4 packets
-	// already.  We do still need the IP layer to compute checksums
-	// correctly, though.
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{
 		ComputeChecksums: true,
@@ -89,11 +67,6 @@ func main() {
 	defer conn.Close()
 	log.Println("writing request")
 	if _, err := conn.WriteTo(buf.Bytes(), &net.IPAddr{IP: dstip}); err != nil {
-		log.Fatal(err)
-	}
-
-	// Set deadline so we don't wait forever.
-	if err := conn.SetDeadline(time.Now().Add(10 * time.Second)); err != nil {
 		log.Fatal(err)
 	}
 
@@ -124,4 +97,34 @@ func main() {
 			log.Printf("Got packet not matching addr")
 		}
 	}
+
+	return
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Printf("Usage: %s <host/ip> <port>\n", os.Args[0])
+		os.Exit(-1)
+	}
+	log.Println("starting")
+
+	// define the seq for this interaction
+	var seq uint32
+	seq = 48901234789
+
+	dstaddrs, err := net.LookupIP(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse the destination host and port from the command line os.Args
+	dstip := dstaddrs[0].To4()
+	var dstport layers.TCPPort
+	if d, err := strconv.ParseUint(os.Args[2], 10, 16); err != nil {
+		log.Fatal(err)
+	} else {
+		dstport = layers.TCPPort(d)
+	}
+
+	sendSyn(dstip, dstport, seq)
 }
