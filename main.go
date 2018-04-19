950d17089676eb55949d3430eb1cf5b438d0c68c
Chris Vantine <justchris.vantine@gmail.com>
2018-04-19 02:57:26 -0400

    Converts layer contents to string

--- main.go
+++ main.go
@@ -163,7 +163,7 @@ func handlePacket(packet gopacket.Packet) () {
 	for _, layer := range packet.Layers() {
 		fmt.Println("PACKET LAYER:", layer.LayerType())
 		if layer.LayerType() == gopacket.LayerTypePayload {
-			fmt.Println(layer.LayerContents())
+			fmt.Println(string(layer.LayerContents()))
 		}
 	}
 }
