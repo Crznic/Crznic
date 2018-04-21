a843c8d575532fd6926a97caf8377ffdbe4ae89f
Milkshak3s <justchris.vantine@gmail.com>
2018-04-21 02:51:56 -0400

    We build TCP now bois

--- main.go
+++ main.go
@@ -1,33 +1,99 @@
 package main
 
 import (
-    "net"
-    "syscall"
-    "encoding/hex"
+  "net"
+  "log"
+  "syscall"
+  "strings"
+  "encoding/hex"
+  "github.com/google/gopacket"
+  "github.com/google/gopacket/layers"
 )
 
 
-func main() {
- // TCP SYN from 192.168.58.128:49394 to 172.217.10.36:80
-  myHex := "005056ea451c000c29c1beea08004500003c8292400040060604c0a83a80acd90a24c0f200503d64c05f00000000a0027210b2540000020405b40402080a001242cd000000000103030a"
-	var pkt []byte
-	pkt, _ = hex.DecodeString(myHex)
+func getLocalIP() net.IP {
+  ifaces, _ := net.Interfaces()
+  var ip net.IP
 
+  for _, i := range ifaces {
+	addrs, _ := i.Addrs()
+	splitAddr := strings.Split(addrs[len(addrs) - 2].String(), "/")
+	ip = net.ParseIP(splitAddr[0])
+  }
+  
+  return ip
+}
+
+
+func sendPacket(pkt []byte, interf string) {
   fd, _ := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
-  if_info, _ := net.InterfaceByName("eth0")
+  if_info, _ := net.InterfaceByName(interf)
 
-    var haddr [8]byte
-    copy(haddr[0:7], if_info.HardwareAddr[0:7])
-    addr := syscall.SockaddrLinklayer{
-        Protocol: syscall.ETH_P_IP,
-        Ifindex:  if_info.Index,
-        Halen:    uint8(len(if_info.HardwareAddr)),
-        Addr:     haddr,
-    }
+  var haddr [8]byte
+  copy(haddr[0:7], if_info.HardwareAddr[0:7])
+  addr := syscall.SockaddrLinklayer{
+	Protocol: syscall.ETH_P_IP,
+	Ifindex:  if_info.Index,
+    Halen:    uint8(len(if_info.HardwareAddr)),
+	Addr:     haddr,
+  }
 
-    syscall.Bind(fd, &addr)
-    syscall.SetLsfPromisc("eth0", true)
-    syscall.Write(fd, pkt)
-    syscall.SetLsfPromisc("eth0", false)
+  syscall.Bind(fd, &addr)
+  syscall.SetLsfPromisc(interf, true)
+  syscall.Write(fd, pkt)
+  syscall.SetLsfPromisc(interf, false)
+}
 
+
+func main() {
+  srcport := layers.TCPPort(80)
+  srcip := getLocalIP()
+
+  dstaddrs, _ := net.LookupIP("10.0.0.1")
+  dstip := dstaddrs[0].To4()
+  dstport := layers.TCPPort(80)
+
+  srcmac, _ := net.ParseMAC("02:00:00:00:00:00")
+  dstmac, _ := net.ParseMAC("04:00:00:00:00:00") 
+
+  ethernet := &layers.Ethernet{
+	SrcMAC:	srcmac,
+	DstMAC:	dstmac,
+	EthernetType: 0x800,
+  }
+  
+  ip := &layers.IPv4{
+	Version:	4,
+	IHL:		5,
+	TOS:		0,
+	Id:			0,
+	Flags:		0,
+	FragOffset:	0,
+	TTL:		225,
+	SrcIP:		srcip,
+	DstIP:		dstip,
+	Protocol:	layers.IPProtocolTCP,
+  }
+
+  tcp := &layers.TCP{
+	SrcPort:	srcport,
+	DstPort:	dstport,
+	Seq:		1,
+	SYN:		true,
+	Window:		14600,
+  }
+  tcp.SetNetworkLayerForChecksum(ip)
+
+  buf := gopacket.NewSerializeBuffer()
+  opts := gopacket.SerializeOptions{
+	ComputeChecksums:	true,
+	FixLengths:		true,
+  }
+  if err := gopacket.SerializeLayers(buf, opts, ethernet, ip, tcp, gopacket.Payload("{'json':'data'}")); err != nil {
+	log.Fatal(err)
+  }
+
+  log.Print(hex.Dump(buf.Bytes()))
+
+  sendPacket(buf.Bytes(), "eth0")
 }
