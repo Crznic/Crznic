a1902524cc0432988cf2f0fbc41c657d5ea51cee
Milkshak3s <justchris.vantine@gmail.com>
2018-04-21 04:03:01 -0400

    Looking more like a library

--- main.go
+++ main.go
@@ -4,30 +4,52 @@ import (
   "net"
   "log"
   "syscall"
-  "strings"
-  "encoding/hex"
   "github.com/google/gopacket"
   "github.com/google/gopacket/layers"
 )
 
 
-func getLocalIP() net.IP {
-  ifaces, _ := net.Interfaces()
-  var ip net.IP
-
-  for _, i := range ifaces {
-	addrs, _ := i.Addrs()
-	splitAddr := strings.Split(addrs[len(addrs) - 2].String(), "/")
-	ip = net.ParseIP(splitAddr[0])
-  }
-  
-  return ip
+type Host struct{
+  Ip		net.IP
+  Mac		net.HardwareAddr
+  Port		layers.TCPPort
 }
 
 
-func sendPacket(pkt []byte, interf string) {
+type Crznic struct {
+  Inter		string
+  Src		*Host
+  Dst		*Host
+  Seq		uint32
+}
+
+
+func newHost(ip net.IP, mac net.HardwareAddr, port layers.TCPPort) *Host {
+  anewHost := &Host{
+	Ip:		ip,
+	Mac:	mac,
+	Port:	port,
+  }
+
+  return anewHost
+}
+
+
+func newCrznic(inter string, src, dst *Host, seq uint32) *Crznic {
+	anewCrznic := &Crznic{
+	  Inter:	inter,
+	  Src:		src,
+	  Dst:		dst,
+	  Seq:		seq,
+	}
+
+	return anewCrznic
+}
+
+
+func (c *Crznic) sendPacket(pkt []byte) {
   fd, _ := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
-  if_info, _ := net.InterfaceByName(interf)
+  if_info, _ := net.InterfaceByName(c.Inter)
 
   var haddr [8]byte
   copy(haddr[0:7], if_info.HardwareAddr[0:7])
@@ -39,26 +61,16 @@ func sendPacket(pkt []byte, interf string) {
   }
 
   syscall.Bind(fd, &addr)
-  syscall.SetLsfPromisc(interf, true)
+  syscall.SetLsfPromisc(c.Inter, true)
   syscall.Write(fd, pkt)
-  syscall.SetLsfPromisc(interf, false)
+  syscall.SetLsfPromisc(c.Inter, false)
 }
 
 
-func main() {
-  srcport := layers.TCPPort(80)
-  srcip := getLocalIP()
-
-  dstaddrs, _ := net.LookupIP("10.0.0.1")
-  dstip := dstaddrs[0].To4()
-  dstport := layers.TCPPort(80)
-
-  srcmac, _ := net.ParseMAC("02:00:00:00:00:00")
-  dstmac, _ := net.ParseMAC("04:00:00:00:00:00") 
-
+func (c *Crznic) sendSYNPacket(payload string) {
   ethernet := &layers.Ethernet{
-	SrcMAC:	srcmac,
-	DstMAC:	dstmac,
+	SrcMAC:	c.Src.Mac,
+	DstMAC:	c.Dst.Mac,
 	EthernetType: 0x800,
   }
   
@@ -70,15 +82,15 @@ func main() {
 	Flags:		0,
 	FragOffset:	0,
 	TTL:		225,
-	SrcIP:		srcip,
-	DstIP:		dstip,
+	SrcIP:		c.Src.Ip,
+	DstIP:		c.Dst.Ip,
 	Protocol:	layers.IPProtocolTCP,
   }
 
   tcp := &layers.TCP{
-	SrcPort:	srcport,
-	DstPort:	dstport,
-	Seq:		1,
+	SrcPort:	c.Src.Port,
+	DstPort:	c.Src.Port,
+	Seq:		c.Seq,
 	SYN:		true,
 	Window:		14600,
   }
@@ -87,13 +99,29 @@ func main() {
   buf := gopacket.NewSerializeBuffer()
   opts := gopacket.SerializeOptions{
 	ComputeChecksums:	true,
-	FixLengths:		true,
+	FixLengths:			true,
   }
-  if err := gopacket.SerializeLayers(buf, opts, ethernet, ip, tcp, gopacket.Payload("{'json':'data'}")); err != nil {
+  if err := gopacket.SerializeLayers(buf, opts, ethernet, ip, tcp, gopacket.Payload(payload)); err != nil {
 	log.Fatal(err)
   }
 
-  log.Print(hex.Dump(buf.Bytes()))
+  c.sendPacket(buf.Bytes())
+}
 
-  sendPacket(buf.Bytes(), "eth0")
+
+func main() {
+  macAddr, _ := net.ParseMAC("0c:02:00:00:00:00")
+  rifs := RoutedInterface("ip", net.FlagUp | net.FlagBroadcast)
+  var dstMac net.HardwareAddr
+  if rifs != nil {
+	dstMac = rifs.HardwareAddr
+  } else {
+	log.Fatal("No")
+  }
+ 
+  srcHost := newHost(net.ParseIP("10.0.0.1"), macAddr, layers.TCPPort(80))
+  dstHost := newHost(net.ParseIP("10.0.0.2"), dstMac, layers.TCPPort(80))
+
+  crz := newCrznic("eth0", srcHost, dstHost, 1)
+  crz.sendSYNPacket("MESSAGE")
 }
