34abdedf383932b19b5c65b3bcba571bd36e66e2
Chris Vantine <justchris.vantine@gmail.com>
2018-04-26 13:39:24 -0400

    Using /proc/net/route to get gateway addr

--- utility.go
+++ utility.go
@@ -6,23 +6,53 @@ import (
 	"bufio"
 	"strings"
 	"errors"
+	"encoding/hex"
 )
 
 func GetRouterMAC() (net.HardwareAddr, error) {
-	f, err := os.Open("/proc/net/arp")
+	// get the default gateway address from routes
+	gatewayAddr := ""
+	fRoute, err := os.Open("/proc/net/route")
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
+	defer fRoute.Close()
 
-	s := bufio.NewScanner(f)
+	s := bufio.NewScanner(fRoute)
 	s.Scan()
 
 	for s.Scan() {
 		line := s.Text()
 		fields := strings.Fields(line)
-		if fields[0] == "gateway" {
-			return net.ParseMAC(fields[1])
+		if fields[1] == "00000000" {
+			decode, err := hex.DecodeString(fields[2])
+			if err != nil {
+				return nil, err
+			}
+
+			gatewayAddr = string(decode[3]) + "." + string(decode[2]) + "." + string(decode[1]) + "." + string(decode[0])
+		}
+	}
+
+	if gatewayAddr == "" {
+		return nil, errors.New("no gateway found in routes")
+	}
+
+	// look through arp tables for match to gateway address
+	fArp, err := os.Open("/proc/net/arp")
+	if err != nil {
+		return nil, err
+	}
+	defer fArp.Close()
+
+	s = bufio.NewScanner(fArp)
+	s.Scan()
+
+	for s.Scan() {
+		line := s.Text()
+		fields := strings.Fields(line)
+		if fields[0] == gatewayAddr {
+			return net.ParseMAC(fields[3])
 		}
 	}
 
