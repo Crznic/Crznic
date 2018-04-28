43ba63abc47c382b3e54a9586626f8589260af09
Nicholas O'Brien <ndo9903@rit.edu>
2018-04-27 23:25:52 -0400

    Added GetMacAddr back

--- utility.go
+++ utility.go
@@ -10,6 +10,22 @@ import (
 	"fmt"
 )
 
+// getMacAddr gets the MAC hardware
+// address of the host machine
+func GetMacAddr() (addr string) {
+	interfaces, err := net.Interfaces()
+	if err == nil {
+		for _, i := range interfaces {
+			if i.Flags&net.FlagUp != 0 && bytes.Compare(i.HardwareAddr, nil) != 0 {
+				// Don't use random as we have a real address
+				addr = i.HardwareAddr.String()
+				break
+			}
+		}
+	}
+	return
+}
+
 func GetRouterMAC() (net.HardwareAddr, error) {
 	// get the default gateway address from routes
 	gatewayAddr := ""
