2eb4309160c64445efab0cb5e1fa8eec11f97c5d
Chris <justchris.vantine@gmail.com>
2018-12-01 18:53:59 -0500

    Fixed spacing in packet.go

--- packet.go
+++ packet.go
@@ -1,65 +1,66 @@
 package crznic
 
 import (
-	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket"
-	"log"
+  "github.com/google/gopacket"
+  "github.com/google/gopacket/layers"
+  "log"
 )
 
 type Packet struct {
-	Ethernet	*layers.Ethernet
-	Ip				*layers.IPv4
-	TCP				*layers.TCP
+  Ethernet *layers.Ethernet
+  Ip       *layers.IPv4
+  TCP      *layers.TCP
 }
 
 func NewPacket(c *Crznic) *Packet {
-	// build ethernet layer
-	ethernet := &layers.Ethernet{
-			SrcMAC:	c.Src.Mac,
-			DstMAC:	c.Dst.Mac,
-			EthernetType: 0x800,
-		}
+  // build ethernet layer
+  ethernet := &layers.Ethernet{
+    SrcMAC:       c.Src.Mac,
+    DstMAC:       c.Dst.Mac,
+    EthernetType: 0x800,
+  }
 
-		// build ip layer
-		ip := &layers.IPv4{
-			Version:		4,
-			IHL:				5,
-			TOS:				0,
-			Id:					0,
-			Flags:			0,
-			FragOffset:	0,
-			TTL:				225,
-			SrcIP:			c.Src.Ip,
-			DstIP:			c.Dst.Ip,
-			Protocol:		layers.IPProtocolTCP,
-		}
+  // build ip layer
+  ip := &layers.IPv4{
+    Version:    4,
+    IHL:        5,
+    TOS:        0,
+    Id:         0,
+    Flags:      0,
+    FragOffset: 0,
+    TTL:        225,
+    SrcIP:      c.Src.Ip,
+    DstIP:      c.Dst.Ip,
+    Protocol:   layers.IPProtocolTCP,
+  }
 
-		// build tcp layer
-		tcp := &layers.TCP{
-			SrcPort:	c.Src.Port,
-			DstPort:	c.Dst.Port,
-			Seq:			c.Seq,
-			Ack:			c.Ack,
-			Window:		14600,
-		}
-		tcp.SetNetworkLayerForChecksum(ip)
+  // build tcp layer
+  tcp := &layers.TCP{
+    SrcPort: c.Src.Port,
+    DstPort: c.Dst.Port,
+    Seq:     c.Seq,
+    Ack:     c.Ack,
+    Window:  14600,
+  }
+  tcp.SetNetworkLayerForChecksum(ip)
 
-		// build base packet
-		packet := &Packet {
-			Ethernet:	ethernet,
-			Ip:				ip,
-			TCP:			tcp,
-		}
+  // build base packet
+  packet := &Packet{
+    Ethernet: ethernet,
+    Ip:       ip,
+    TCP:      tcp,
+  }
 
-		return packet
+  return packet
 }
 
 func (p *Packet) Serialize(buf gopacket.SerializeBuffer, payload string) {
-	opts := gopacket.SerializeOptions{
-		ComputeChecksums:	true,
-		FixLengths:				true,
-	}
-	if err := gopacket.SerializeLayers(buf, opts, p.Ethernet, p.Ip, p.TCP, gopacket.Payload(payload)); err != nil {
-		log.Fatal(err)
-	}
+  opts := gopacket.SerializeOptions{
+    ComputeChecksums: true,
+    FixLengths:       true,
+  }
+
+  if err := gopacket.SerializeLayers(buf, opts, p.Ethernet, p.Ip, p.TCP, gopacket.Payload(payload)); err != nil {
+    log.Fatal(err)
+  }
 }
